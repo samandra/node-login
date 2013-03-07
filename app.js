3d0d33abb0f564c3886f25ef31678e302cf6d3e3
mustafa dikici <mus.dikici@gmail.com>
2013-03-07 19:44:39 +0200

    added authenticate for mongohq

--- app.js
+++ app.js
@@ -14,14 +14,15 @@ app.configure(function(){
 	app.set('views', __dirname + '/app/server/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
-//	app.use(express.favicon());
-//	app.use(express.logger('dev'));
+
 	app.use(express.bodyParser());
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: 'super-duper-secret-secret' }));
 	app.use(express.methodOverride());
 	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
 	app.use(express.static(__dirname + '/app/public'));
+
+	app.use(express.errorHandler({dumpExceptions: true, showStack: true})); // Dump errors
 });
 
 app.configure('development', function(){
@@ -32,4 +33,4 @@ require('./app/server/router')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
-})
+});
